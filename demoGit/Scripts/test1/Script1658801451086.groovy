e6d4c8af92cd513c2f40f3bd62ae08a2c69a786a
NghiaCT <trungnghia.ctn94@gmail.com>
2022-07-26 11:58:52 +0700

    v1_2020

--- demoGit/Scripts/test1/Script1658801451086.groovy
+++ demoGit/Scripts/test1/Script1658801451086.groovy
@@ -17,5 +17,6 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 'nghia check'
+'v1_2020 change'
 
 
